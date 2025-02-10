a89c88368e528b756b60e23039f211a493f781e7
Riddhi Patel <39990641+RiddhiBhadani@users.noreply.github.com>
2025-02-10 18:31:24 -0500

    adding code for deploy windows build - using Nexus

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,10 +6,10 @@ def UNITY_INSTALLATION = "C:\\Program Files\\Unity\\Hub\\Editor\\${UNITY_VERSION
 pipeline{
     environment{
         PROJECT_PATH = "${CUSTOM_WORKSPACE}"
-        /*
-        NEXUS_IP_ADDRESS = "" //Your full Nexus IP address+port. Example: http://192.168.1.200:8081
-        NEXUS_USERNAME = "" //Your Nexus username
+        NEXUS_IP_ADDRESS = "http://192.168.2.38:8081" //Your full Nexus IP address+port. Example: http://192.168.1.200:8081
+        NEXUS_USERNAME = "riddhi" //Your Nexus username
         NEXUS_PASSWORD = credentials('NEXUS_PASSWORD')
+        /*
         MAC_PASSWORD = credentials('MAC_PASSWORD')
 
         GOOGLE_PLAY_API_JSON_LOCATION = credentials('GOOGLE_PLAY_API_JSON_LOCATION')
@@ -19,16 +19,11 @@ pipeline{
         ALIAS_PASS = credentials('ALIAS_PASS')
         BUNDLE_ID = "" //Your bundle ID. Ex: com.defaultcompany.test
         MAC_IP_ADDRESS = "" //Your mac IP address
-        MAC_USERNAME = "" //Your mac username*/
+        MAC_USERNAME = "" //Your mac username
+        */
     }
 
     agent any
-        // label 
-        // // label{
-        // //     label ""
-        // //     customWorkspace "${CUSTOM_WORKSPACE}"
-        // // }
-    
 
     stages{
         stage('Git Version Check') {
@@ -51,6 +46,20 @@ pipeline{
                         "%UNITY_PATH%\\Unity.exe" -quit -batchmode -projectPath %PROJECT_PATH% -executeMethod BuildScript.BuildWindows -logFile -
                         '''
                     }
+                }
+            }
+        }
+
+     stage('Deploy Windows'){
+            when{expression {DEPLOY_WINDOWS == 'true'}}
+            steps{
+                script{
+                    def buildDate = new Date().format("yyyyMMdd_HHmm")
+                    env.ARTIFACT_NAME = "Windows_Build_${buildDate}.zip"
+
+                    bat '''
+                    curl -u %NEXUS_USERNAME%:%NEXUS_PASSWORD% --upload-file %PROJECT_PATH%/Builds/Windows.zip %NEXUS_IP_ADDRESS%/repository/jenkins_unity_test/Windows_Builds/%ARTIFACT_NAME%
+                    '''
                 }
             }
         }
