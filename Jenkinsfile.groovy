76032d81a2135babb81d36afb38a25c03c132a3e
Riddhi Patel <39990641+RiddhiBhadani@users.noreply.github.com>
2025-02-10 01:42:43 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,6 +6,7 @@ def UNITY_INSTALLATION = "C:\\Program Files\\Unity\\Hub\\Editor\\${UNITY_VERSION
 pipeline{
     environment{
         PROJECT_PATH = "${CUSTOM_WORKSPACE}\\${PROJECT_NAME}"
+        /*
         NEXUS_IP_ADDRESS = "" //Your full Nexus IP address+port. Example: http://192.168.1.200:8081
         NEXUS_USERNAME = "" //Your Nexus username
         NEXUS_PASSWORD = credentials('NEXUS_PASSWORD')
@@ -18,7 +19,7 @@ pipeline{
         ALIAS_PASS = credentials('ALIAS_PASS')
         BUNDLE_ID = "" //Your bundle ID. Ex: com.defaultcompany.test
         MAC_IP_ADDRESS = "" //Your mac IP address
-        MAC_USERNAME = "" //Your mac username
+        MAC_USERNAME = "" //Your mac username*/
     }
 
     agent{
@@ -42,19 +43,6 @@ pipeline{
             }
         }
 
-        stage('Deploy Windows'){
-            when{expression {DEPLOY_WINDOWS == 'true'}}
-            steps{
-                script{
-                    def buildDate = new Date().format("yyyyMMdd_HHmm")
-                    env.ARTIFACT_NAME = "Windows_Build_${buildDate}.zip"
-
-                    bat '''
-                    curl -u %NEXUS_USERNAME%:%NEXUS_PASSWORD% --upload-file %PROJECT_PATH%/Builds/Windows.zip %NEXUS_IP_ADDRESS%/repository/jenkins_unity_test/Windows_Builds/%ARTIFACT_NAME%
-                    '''
-                }
-            }
-        }
         stage('Build WebGL'){
             when{expression {BUILD_WEBGL == 'true'}}
             steps{
@@ -68,21 +56,8 @@ pipeline{
             }
         }
 
-        stage('Deploy WebGL'){
-            when{expression {DEPLOY_WEBGL == 'true'}}
-            steps{
-                script{
-                    def buildDate = new Date().format("yyyyMMdd_HHmm")
-                    env.ARTIFACT_NAME = "WebGL_Build_${buildDate}.zip"
-
-                    bat '''
-                    curl -u %NEXUS_USERNAME%:%NEXUS_PASSWORD% --upload-file %PROJECT_PATH%/Builds/WebGL.zip %NEXUS_IP_ADDRESS%/repository/jenkins_unity_test/WebGL_Builds/%ARTIFACT_NAME%
-                    '''
-                }
-            }
-        }
-
-        stage('Build Android APK'){
+        stage('Build Android APK')
+        {
             when{expression {BUILD_ANDROID_APK == 'true'}}
             steps{
                 script{
@@ -91,108 +66,6 @@ pipeline{
                         "%UNITY_PATH%/Unity.exe" -quit -batchmode -projectPath %PROJECT_PATH% -executeMethod BuildScript.BuildAndroid -buildType APK -logFile -
                         '''
                     }
-                }
-            }
-        }
-
-        stage('Deploy Android APK - Nexus'){
-            when{expression {DEPLOY_ANDROID_APK == 'true'}}
-            steps{
-                script{
-                    def buildDate = new Date().format("yyyyMMdd_HHmm")
-                    env.ARTIFACT_NAME = "Android_Build_${buildDate}.apk"
-
-                    bat '''
-                    curl -u %NEXUS_USERNAME%:%NEXUS_PASSWORD% --upload-file %PROJECT_PATH%/Builds/AndroidAPK/TestGame.apk %NEXUS_IP_ADDRESS%/repository/jenkins_unity_test/AndroidAPK_Builds/%ARTIFACT_NAME%
-                    '''
-                }
-            }
-        }
-
-        stage('Build Android AAB'){
-            when{expression {BUILD_ANDROID_AAB == 'true'}}
-            steps{
-                script{
-                    withEnv(["UNITY_PATH=${UNITY_INSTALLATION}"]){
-                        bat '''
-                        "%UNITY_PATH%/Unity.exe" -quit -batchmode -projectPath %PROJECT_PATH% -executeMethod BuildScript.BuildAndroid -buildType AAB -logFile -
-                        '''
-                    }
-                }
-            }
-        }
-
-        stage('Deploy Android AAB - Nexus'){
-            when{expression {DEPLOY_ANDROID_AAB == 'true'}}
-            steps{
-                script{
-                    def buildDate = new Date().format("yyyyMMdd_HHmm")
-                    env.ARTIFACT_NAME = "Android_Build_${buildDate}.aab"
-
-                    bat '''
-                    curl -u %NEXUS_USERNAME%:%NEXUS_PASSWORD% --upload-file %PROJECT_PATH%/Builds/AndroidAAB/TestGame.aab %NEXUS_IP_ADDRESS%/repository/jenkins_unity_test/AndroidAAB_Builds/%ARTIFACT_NAME%
-                    '''
-                }
-            }
-        }
-
-        stage('Deploy Android - Google Play'){
-            when{expression {DEPLOY_ANDROID_AAB == 'true'}}
-            steps{
-                script{
-                    bat '''
-                    fastlane supply ^
-                    --track internal ^
-                    --release_status "draft" ^
-                    --aab "%PROJECT_PATH%/Builds/AndroidAAB/TestGame.aab" ^
-                    --json_key "%GOOGLE_PLAY_API_JSON_LOCATION%" ^
-                    --package_name "%BUNDLE_ID%" ^
-                    --version_name "V%BUILD_NUMBER% - Test Project"
-                    '''
-                }
-            }
-        }
-
-        stage('Build iOS'){
-            when{expression {BUILD_IOS == 'true'}}
-            steps{
-                script{
-                    withEnv(["UNITY_PATH=${UNITY_INSTALLATION}"]){
-                        bat '''
-                        "%UNITY_PATH%/Unity.exe" -quit -batchmode -projectPath %PROJECT_PATH% -executeMethod BuildScript.BuildIOS -logFile -
-                        '''
-                    }
-                }
-            }
-        }
-
-        stage('Deploy iOS Nexus'){
-            when{expression {DEPLOY_IOS_NEXUS == 'true'}}
-            steps{
-                script{
-                    def buildDate = new Date().format("yyyyMMdd_HHmm")
-                    env.ARTIFACT_NAME = "iOS_Build_${buildDate}.zip"
-
-                    bat '''
-                    curl -u %NEXUS_USERNAME%:%NEXUS_PASSWORD% --upload-file %PROJECT_PATH%/Builds/iOS.zip %NEXUS_IP_ADDRESS%/repository/jenkins_unity_test/iOS_Builds/%ARTIFACT_NAME%
-                    '''
-                }
-            }
-        }
-
-        stage('Deploy iOS Mac'){
-            when{expression {DEPLOY_IOS_MAC == 'true'}}
-            steps{
-                script{
-                    env.PROJECT_NAME = PROJECT_NAME
-
-                    powershell '''
-                    net use \\\\%MAC_IP_ADDRESS% /user:%MAC_USERNAME% $env:MAC_PASSWORD
-                    Remove-Item -Path \\\\%MAC_IP_ADDRESS%\\%MAC_USERNAME%\\Desktop\\Jenkins_Builds\\$env:PROJECT_NAME -Recurse -Force -ErrorAction Ignore
-                    New-Item -ItemType directory -Path \\\\%MAC_IP_ADDRESS%\\%MAC_USERNAME%\\Desktop\\Jenkins_Builds\\$env:PROJECT_NAME -Force
-                    Copy-Item -Path "$env:PROJECT_PATH\\Builds\\iOS" -Destination \\\\%MAC_IP_ADDRESS%\\%MAC_USERNAME%\\Desktop\\Jenkins_Builds\\$env:PROJECT_NAME -Recurse -Force
-                    net use \\\\%MAC_IP_ADDRESS% /delete
-                    '''
                 }
             }
         }
